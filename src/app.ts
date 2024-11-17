8881e9427ef277db60bb0b80b4111defeda84031
syairabi <syairazishaharudin@gmail.com>
2024-11-18 00:15:54 +0800

    update
    add ejs

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/database';
 import authRoutes from './routes/auth';
+import { join } from 'path';
+import fileRoutes from './routes/file';
+import maintenanceRoutes from './routes/maintenance';
 
 // Load environment variables
 dotenv.config();
@@ -12,6 +15,8 @@ const app = express();
 
 // Middleware to parse JSON requests
 app.use(express.json());
+app.set('views', join(__dirname, '..', 'views'));
+app.use(express.static(join(__dirname, '..', 'public')))
 
 // Connect to MongoDB
 connectDB();
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,6 +3,10 @@ import { AuthController } from '../controllers/authController';
 
 const router = express.Router();
 
+router.get('/login', (req, res)=>{
+    res.render('login.ejs');
+})
+
 // Use static methods directly from the class
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
