77f794e1f6eea6855a4ac4b01983e840be2752eb
syairabi <88481622+syairabi@users.noreply.github.com>
2024-11-22 16:21:02 +0800

    add ui
    update home ui and add file page

--- src/routes/file.ts
+++ src/routes/file.ts
@@ -24,6 +24,10 @@ router.get('/homepage', (req, res)=>{
   res.render('homepage.ejs');
 })
 
+router.get('/file', (req, res)=>{
+  res.render('file.ejs');
+})
+
 router.post('/upload', auth, upload.single('file'), FileController.uploadFile);
 router.get('/', auth, FileController.getAllFiles);
 router.get('/download/:id', auth, FileController.downloadFile);
